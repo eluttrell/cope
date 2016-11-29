11e8ae15491e0f741d856afc25380e62cf938836
Elijah <elieel@gmail.com>
2016-11-29 14:07:22 -0500

    Complete signup process (HTML, JS, NODE, bcrypt, db)

--- backend.js
+++ backend.js
@@ -2,59 +2,36 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
-    nicknames = [];
+    bcrypt = require('bcrypt'),
+    bodyParser = require('body-parser'),
+    pgp = require('pg-promise')();
+    // nicknames = [];
 
-server.listen(3000);
+// Run this on terminal to get node modules:
+/*npm install --save express@4.10.2*/
+/*npm install --save socket.io*/
+
 
 // Makes front end files pull from /public
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
-// This is no longer necessary ßbecause of line above
-// app.get('/', function(req, res) {
-//   res.sendfile(__dirname + '/index.html');
-// });
+server.listen(3000);
 
-// This logs user connection status to the console and assigns nicknames (usernames)
-io.on('connection', function(socket) {
-  socket.on('new user', function(data, callback) {
-    if (nicknames.indexOf(data) != -1) {
-      callback(false);
+var db = pgp(database='cope_db');
+
+db.connect();
+
+app.post('/signup', function(req, res) {
+  var userInfo = req.body;
+  // console.log("THIS THIS THIS:" + userInfo);
+  bcrypt.hash(userInfo.password, 10, function(err, hash) {
+    if (err) {
+      res.json({status: "Failed"});
+      return;
     } else {
-      callback(true);
-      socket.nickname = data;
-      nicknames.push(socket.nickname);
-      io.sockets.emit('usernames', nicknames);
+      db.query('INSERT INTO copee VALUES(default, $1, $2, $3, $4, $5, default, default)', [userInfo.username, userInfo.email, hash, userInfo.first_name, userInfo.last_name]);
     }
+    res.json({status: "OK"});
   });
-
-// This logs a user disconnect to the console
-  console.log('a user connected');
-  socket.on('disconnect', function() {
-    console.log('user disconnect');
-  });
-
-  function updateNicknames() {
-    io.sockets.emit('usernames', nicknames);
-  }
-
-  socket.on('disconnect', function(data) {
-    if(!socket.nickname) return;
-    nicknames.splice(nicknames.indexOf(socket.nickname), 1);
-    updateNicknames();
-  })
-});
-
-// This logs user's sent messages to the console along with the current username of the user
-io.on('connection', function(socket) {
-  socket.on('send message', function(data) {
-    console.log(socket.nickname + "'s message: " + data)
-  })
-})
-
-// This sends the message from the server to the rest of the users. If you wanted to send it to everyone except the sender (or a specific socket), you would use socket.broadcast.emit('new message', data). Then on the client side (inside index.html) we will capture the 'new message' event and include it on the page.
-io.sockets.on('connection', function(socket) {
-    socket.on('send message', function(data) {
-
-        io.sockets.emit('new message', {data: data, username: socket.nickname});
-    });
 });
